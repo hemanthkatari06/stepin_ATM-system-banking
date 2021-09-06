7e3b8bfe2f779a70a64fca85983f791e1330461b
hemanthkatari06 <89626453+hemanthkatari06@users.noreply.github.com>
2021-09-06 17:10:50 +0530

    initial commit

---
+++ header.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+//Functions
+
+void login();
+void mainMenu();
+void checkBalance(float balance);
+float moneyDeposit(float balance);
+float moneyWithdraw(float balance);
+void menuExit();
+void errorMessage();
